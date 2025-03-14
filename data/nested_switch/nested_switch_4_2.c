d9ddc1cc40e77bf1288f370974377d61eb67c334
sangjun19 <sangjun0319@gmail.com>
2025-03-14 15:48:35 +0900

    src: add switch generator(nested, computational)

---
+++ data/nested_switch/nested_switch_4_2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int value = 4, sub_value = 2;
+    switch (value) {
+        case 1:
+            switch (sub_value) {
+                case 1:
+                    printf("1-1 실행됨\n");
+                    break;
+                case 2:
+                    printf("1-2 실행됨\n");
+                    break;
+            }
+            break;
+        case 2:
+            switch (sub_value) {
+                case 1:
+                    printf("2-1 실행됨\n");
+                    break;
+                case 2:
+                    printf("2-2 실행됨\n");
+                    break;
+            }
+            break;
+        default:
+            printf("기본값 실행됨\n");
+            break;
+    }
+    return 0;
+}
