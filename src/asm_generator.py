703b488c61e947d4f35c23521a28e24bf677a0f2
AnSangjun <sangjun0319@gmail.com>
2025-04-04 17:07:03 +0900

    src: modify asm_generator.py

--- src/asm_generator.py
+++ src/asm_generator.py
@@ -9,11 +9,9 @@ def compile_to_assembly(filename):
     base_filename = os.path.splitext(os.path.basename(filename))[0]
 
     # 어셈블리 코드를 저장할 디렉토리
-<<<<<<< HEAD
-    output_dir = f"./data/assembly_code/{file_name}"
-=======
+
     output_dir = f"./data/{file_name}_asm"
->>>>>>> 5175742c82fb47275e31077cbfc274fefa466250
+
 
     # GCC로 어셈블리 코드 생성
     compile_command = ["gcc", "-S", filename, "-o", f"{output_dir}/{base_filename}.s"]
@@ -28,8 +26,5 @@ c_files = [f"./data/3d_switch/{file_name}_{i}.c" for i in range(1, 1001)]
 
 # 각 C 파일을 어셈블리 코드로 변환
 for filename in c_files:
-<<<<<<< HEAD
+
     compile_to_assembly(filename)
-=======
-    compile_to_assembly(filename)
->>>>>>> 5175742c82fb47275e31077cbfc274fefa466250
