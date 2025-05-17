fa6e2faaf5d636fe3c3fccb7de75d25e8df7bcd4
sangjun19 <sangjun0319@gmail.com>
2025-05-17 14:20:00 +0900

    media: 발표 영상 추가
    테스트 계획서 영상 링크 추가

---
+++ data/raw_code/switch/3d_switch/3d_switch_406 5.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+
+int main() {
+    int value_1 = 5;
+    int value_2 = 1;
+    int value_3 = 6;
+    switch (value_1) {
+        case 1:
+            switch (value_2) {
+                case 1:
+                    switch (value_3) {
+                        case 1:
+                            printf("1-1-1\n");
+                            break;
+                        case 2:
+                            printf("1-1-2\n");
+                            break;
+                        case 3:
+                            printf("1-1-3\n");
+                            break;
+                        case 4:
+                            printf("1-1-4\n");
+                            break;
+                        case 5:
+                            printf("1-1-5\n");
+                            break;
+                        case 6:
+                            printf("1-1-6\n");
+                            break;
+                    }
+                    break;
+            }
+            break;
+        case 2:
+            switch (value_2) {
+                case 1:
+                    switch (value_3) {
+                        case 1:
+                            printf("2-1-1\n");
+                            break;
+                        case 2:
+                            printf("2-1-2\n");
+                            break;
+                        case 3:
+                            printf("2-1-3\n");
+                            break;
+                        case 4:
+                            printf("2-1-4\n");
+                            break;
+                        case 5:
+                            printf("2-1-5\n");
+                            break;
+                        case 6:
+                            printf("2-1-6\n");
+                            break;
+                    }
+                    break;
+            }
+            break;
+        case 3:
+            switch (value_2) {
+                case 1:
+                    switch (value_3) {
+                        case 1:
+                            printf("3-1-1\n");
+                            break;
+                        case 2:
+                            printf("3-1-2\n");
+                            break;
+                        case 3:
+                            printf("3-1-3\n");
+                            break;
+                        case 4:
+                            printf("3-1-4\n");
+                            break;
+                        case 5:
+                            printf("3-1-5\n");
+                            break;
+                        case 6:
+                            printf("3-1-6\n");
+                            break;
+                    }
+                    break;
+            }
+            break;
+        case 4:
+            switch (value_2) {
+                case 1:
+                    switch (value_3) {
+                        case 1:
+                            printf("4-1-1\n");
+                            break;
+                        case 2:
+                            printf("4-1-2\n");
+                            break;
+                        case 3:
+                            printf("4-1-3\n");
+                            break;
+                        case 4:
+                            printf("4-1-4\n");
+                            break;
+                        case 5:
+                            printf("4-1-5\n");
+                            break;
+                        case 6:
+                            printf("4-1-6\n");
+                            break;
+                    }
+                    break;
+            }
+            break;
+        case 5:
+            switch (value_2) {
+                case 1:
+                    switch (value_3) {
+                        case 1:
+                            printf("5-1-1\n");
+                            break;
+                        case 2:
+                            printf("5-1-2\n");
+                            break;
+                        case 3:
+                            printf("5-1-3\n");
+                            break;
+                        case 4:
+                            printf("5-1-4\n");
+                            break;
+                        case 5:
+                            printf("5-1-5\n");
+                            break;
+                        case 6:
+                            printf("5-1-6\n");
+                            break;
+                    }
+                    break;
+            }
+            break;
+    }
+    return 0;
+}
