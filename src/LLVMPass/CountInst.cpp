b5de0b23eafb4bd4ec5da05a0c39406680542eb9
AnSangjun <sangjun0319@gmail.com>
2025-10-17 11:21:46 +0900

    feat: Add counting instruction pass

---
+++ src/LLVMPass/CountInst.cpp
@@ -0,0 +1,40 @@
+// InstCountPass.cpp
+#include "llvm/IR/PassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+namespace {
+struct CountInstPass : PassInfoMixin<CountInstPass> {
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
+    errs() << "Running CountInstPass on " << F.getName() << "\n";
+    
+    unsigned instCount = 0;
+    
+    for (BasicBlock &BB : F) {
+      instCount += BB.size();
+    }
+    
+    errs() << "Instruction count for " << F.getName()  << ": " << instCount << "\n";
+    return PreservedAnalyses::all();
+  }
+};
+}
+
+extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo llvmGetPassPluginInfo() {
+  return {
+      LLVM_PLUGIN_API_VERSION, "CountInstPass", LLVM_VERSION_STRING,
+      [](PassBuilder &PB) {
+        PB.registerPipelineParsingCallback(
+            [](StringRef Name, FunctionPassManager &FPM,
+               ArrayRef<PassBuilder::PipelineElement>) {
+              if (Name == "countinst") {
+                FPM.addPass(CountInstPass());
+                return true;
+              }
+              return false;
+            });
+      }};
+}
